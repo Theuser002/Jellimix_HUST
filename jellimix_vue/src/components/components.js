4dca0d0def099a47e5c41cd92e17bd9787cc836c
phihungtran <phihung99nd@gmail.com>
2021-11-18 10:58:41 +0700

    register component

--- jellimix_vue/src/components/components.js
+++ jellimix_vue/src/components/components.js
@@ -3,7 +3,34 @@ const TheMenu = () =>
     import ('../components/TheMenu.vue')
 const TheHeader = () =>
     import ('../components/TheHeader.vue')
+const TheFooter = () =>
+    import ('../components/TheFooter.vue')
+const TheBanner = () =>
+    import ('../components/MediaBlock/TheBanner.vue')
+const TheFeaturedAlbum = () =>
+    import ('../components/MediaBlock/TheFeaturedAlbum.vue')
+const TheFeaturedArtist = () =>
+    import ('../components/MediaBlock/TheFeaturedArtist.vue')
+const TheFeaturedSong = () =>
+    import ('../components/MediaBlock/TheFeaturedSong.vue')
+const TheRecentPlayed = () =>
+    import ('../components/MediaBlock/TheRecentPlayed.vue')
+const TheWeeklyTop = () =>
+    import ('../components/MediaBlock/TheWeeklyTop.vue')
+const PrimaryMedia = () =>
+    import ('../components/MediaBlock/Media/PrimaryMedia.vue')
+const MiniMedia = () =>
+    import ('../components/MediaBlock/Media/MiniMedia.vue')
 
 //Global components
 Vue.component('TheMenu', TheMenu);
 Vue.component('TheHeader', TheHeader);
+Vue.component('TheFooter', TheFooter);
+Vue.component('TheBanner', TheBanner);
+Vue.component('TheFeaturedAlbum', TheFeaturedAlbum);
+Vue.component('TheFeaturedArtist', TheFeaturedArtist);
+Vue.component('TheFeaturedSong', TheFeaturedSong);
+Vue.component('TheRecentPlayed', TheRecentPlayed);
+Vue.component('TheWeeklyTop', TheWeeklyTop);
+Vue.component('PrimaryMedia', PrimaryMedia);
+Vue.component('MiniMedia', MiniMedia);
