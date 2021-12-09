439bbbb4a495bbb6b0255fde376e46f33fd239d4
phihungtran <phihung99nd@gmail.com>
2021-12-09 15:16:56 +0700

    add media to song page

--- jellimix_vue/src/views/SongPage.vue
+++ jellimix_vue/src/views/SongPage.vue
@@ -1,19 +1,49 @@
 <template>
-    <div class="song">
-        <a @click="previousRoute">Back</a>
+  <div class="song">
+    <a @click="previousRoute">Back</a>
+    <div class="feature-album">
+      <div class="ms_rcnt_slider">
+        <div class="ms_heading">
+          <h1>Featured Album</h1>
+        </div>
+        <div style="display: flex; flex-direction: row" v-for="(num) in [0,1,2]" :key="num">
+          <PrimaryMedia
+            v-for="(item, index) in mediaList.slice(num*4,num*4+4)"
+            :key="index"
+            :media_data="item"
+          />
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import SongServices from "../common/SongServices";
+
 export default {
-    methods: {
-        previousRoute(){
-            this.$router.back()
-        }
+  mixins: [SongServices],
+  data() {
+    return {
+      mediaList: [],
+    };
+  },
+  created() {
+    this.getAllSong()
+      .then((res) => {
+        this.mediaList = res.data.Items;
+      })
+      .catch((res) => {
+        console.log(res);
+      });
+  },
+  methods: {
+    previousRoute() {
+      this.$router.back();
     },
-}
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
