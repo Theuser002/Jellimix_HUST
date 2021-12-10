8fc65eee0993dd23ea72cdb956714d6fd36b9e4c
phucnguyen111 <phucnguyen0111@gmail.com>
2021-12-10 11:29:10 +0700

    restyle menu

--- jellimix_vue/src/components/TheMenu.vue
+++ jellimix_vue/src/components/TheMenu.vue
@@ -1,20 +1,22 @@
 <template>
   <div
-    class="sidemenu_wrapper"
+    class="ms_sidemenu_wrapper"
     :class="{
       open_menu: isOpenMenu,
     }"
   >
-    <div class="sidemenu_toggle" @click="isOpenMenu = !isOpenMenu"></div>
+    <div class="ms_nav_close" @click="isOpenMenu = !isOpenMenu">
+      <i class="fa fa-angle-right" aria-hidden="true"></i>
+    </div>
     <!-- Nút đóng mở menu -->
-    <div class="sidemenu_inner">
+    <div class="ms_sidemenu_inner">
       <div
-        class="sidemenu-logo_wrapper"
+        class="ms_logo_inner"
         :class="{
           open_menu: isOpenMenu,
         }"
       >
-        <div class="sidemenu-logo" v-if="!isOpenMenu">
+        <div class="ms_logo" v-if="!isOpenMenu">
           <!-- Icon khi đóng menu -->
           <router-link to="/Home">
             <img
@@ -25,7 +27,7 @@
             />
           </router-link>
         </div>
-        <div class="sidemenu-logo_open" v-else>
+        <div class="ms_logo_open" v-else>
           <!-- Icon khi mở menu -->
           <router-link to="/Home">
             <img
@@ -37,26 +39,22 @@
           </router-link>
         </div>
       </div>
-      <div class="sidemenu-item_wrapper">
-        <div
-          class="sidemenu-item"
-          v-for="(item, index) in menuItems"
-          :key="index"
-          @click="isOpenMenu = false"
-        >
-          <!--Loop qua các lựa chọn trong menu-->
-          <router-link :to="item.path">
-            <span class="item-icon">
-              <span
-                class="icon"
-                :style="{
-                  'background-position': item.x + 'px ' + item.y + 'px',
-                }"
-              ></span>
-            </span>
-            <span class="item-text">{{ item.name }}</span> </router-link
-          ><br />
-        </div>
+      <div class="sidemenu-item_wrapper ms_nav_wrapper">
+        <ul>
+          <li
+            v-for="(item, index) in menuItems"
+            :key="index"
+            @click="isOpenMenu = false"
+          >
+            <!--Loop qua các lựa chọn trong menu-->
+            <router-link :to="item.path" active-class="active ">
+              <span class="nav_icon">
+                <span class="icon" :class="item.icon"></span>
+              </span>
+              <span class="nav_text">{{ item.name }}</span>
+            </router-link>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -68,11 +66,11 @@ export default {
     return {
       isOpenMenu: false,
       menuItems: [
-        { path: "/Home/dev", name: "Dev", x: 0, y: 0 },//đường dẫn router, tên lựa chọn, tọa độ x-y của icon trong ảnh svg
-        { path: "/Home", name: "Trang chủ", x: 0, y: 0 },//đường dẫn router, tên lựa chọn, tọa độ x-y của icon trong ảnh svg
-        { path: "/Song", name: "Bài hát", x: -150, y: -28 },
-        { path: "/Artist", name: "Nghệ sĩ", x: -60, y: 2 },
-        { path: "/Album", name: "Album", x: -30, y: -28 },
+        { path: "/Home/dev", name: "Dev", icon: "icon_discover" }, //đường dẫn router, tên lựa chọn, tọa độ x-y của icon trong ảnh svg
+        { path: "/Home", name: "Trang chủ", icon: "icon_discover" }, //đường dẫn router, tên lựa chọn, tọa độ x-y của icon trong ảnh svg
+        { path: "/Song", name: "Bài hát", icon: "icon_music" },
+        { path: "/Artist", name: "Nghệ sĩ", icon: "icon_artists" },
+        { path: "/Album", name: "Album", icon: "icon_albums" },
       ],
     };
   },
