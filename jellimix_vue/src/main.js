d570eb6f4898f69dc0d357100e5db288b35127a2
phucnguyen111 <phucnguyen0111@gmail.com>
2021-12-10 10:31:12 +0700

    init album page

--- jellimix_vue/src/main.js
+++ jellimix_vue/src/main.js
@@ -25,7 +25,7 @@ Vue.use(VueToast, {
 });
 
 
-Vue.use(VueAwesomeSwiper, /* { default options with global component } */ )
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 
 new Vue({
     router,
