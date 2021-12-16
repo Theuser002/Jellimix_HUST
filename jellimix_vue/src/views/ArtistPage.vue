a16a87f9436b3f227aef4cf1606a9dce84548e04
I001user <hung.nt176773@sis.hust.edu.vn>
2021-12-16 22:14:35 +0700

    artist description added

--- jellimix_vue/src/views/ArtistPage.vue
+++ jellimix_vue/src/views/ArtistPage.vue
@@ -1,8 +1,35 @@
 <template>
-<div>Trang danh sách nghệ sĩ</div>    
+   <div class="album_single_data">
+                <div class="album_single_img">
+                    <img src="images/featured/song1.jpg" alt="artist profile pic" class="img-fluid">
+                </div>
+                <div class="album_single_text">
+                    <h2> &lt;Artist name&gt; </h2>
+                    <p class="singer_name">Singer, New York</p>
+                    <div class="about_artist">
+                        &lt;The artist&gt; seems to be a great person so he is loved by many people but all is just the big, shiny, flashy friendly facade he puts on. &lt;The artist&gt; is in fact a psychopath and was convited of serial m.u.r.d.e.r of 13 victims and is now waiting on death row in Volatile prison. His music is good,he is not. Here are some...<a href="#">Read More</a>
+                    </div>
+                    <div class="album_btn">
+                        <a href="#" class="ms_btn play_btn"><span class="play_all"><img src="images/svg/play_all.svg" alt="">Play All</span><span class="pause_all"><img src="images/svg/pause_all.svg" alt="">Pause</span></a>
+                        <a href="#" class="ms_btn"><span class="play_all"><img src="images/svg/add_q.svg" alt="">Add To Queue</span></a>
+                    </div>
+                </div>
+                <div class="album_more_optn ms_more_icon">
+                    <span><img src="images/svg/more.svg" alt=""></span>
+                </div>
+                <ul class="more_option">
+                    <li><a href="#"><span class="opt_icon"><span class="icon icon_fav"></span></span>Add To Favourites</a></li>
+                    <li><a href="#"><span class="opt_icon"><span class="icon icon_queue"></span></span>Add To Queue</a></li>
+                    <li><a href="#"><span class="opt_icon"><span class="icon icon_dwn"></span></span>Download Now</a></li>
+                    <li><a href="#"><span class="opt_icon"><span class="icon icon_playlst"></span></span>Add To Playlist</a></li>
+                    <li><a href="#"><span class="opt_icon"><span class="icon icon_share"></span></span>Share</a></li>
+                </ul>
+            </div>
 </template>
 
 <script>
+// import axios from axios
+
 export default {
     
 }
