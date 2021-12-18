a8ee427300c393f4bcb9ff2f55c62a57665968f0
phihungtran <phihung99nd@gmail.com>
2021-12-18 11:09:01 +0700

    add loader to artist page, send artist info to single artist page

--- jellimix_vue/src/common/ArtistServices.js
+++ jellimix_vue/src/common/ArtistServices.js
@@ -13,6 +13,12 @@ var ArtistServices = {
                 `EnableImageTypes=Primary%2CBackdrop%2CBanner%2CThumb&` +
                 `Limit=100&ParentId=7e64e319657a9516ec78490da03edccb&userId=${user}&api_key=${api_key}`
             return axios.get(url);
+        },
+        async getSingleArtist(id) {
+            let url = axios.defaults.baseURL +
+                `Users/${user}/Items/${id}?` +
+                `api_key=${api_key}`
+            return await axios.get(url);
         }
     },
 }
